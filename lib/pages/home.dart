0a1869c7b4aa86b6ad69add700f70655a86ef60a
Sidiq Muhammad Toha <sidiqtoha2000@gmail.com>
2024-03-05 15:49:27 +0700

    add

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:pdf_apps/service/pdf_service.dart';
 
 class HomePages extends StatefulWidget {
@@ -16,17 +13,8 @@ class _HomePagesState extends State<HomePages> {
 
   List _orders = [];
 
-  // Future<void> readJson() async {
-  //   final String response = await rootBundle.loadString('assets/orders.json');
-  //   final data = await json.decode(response);
-  //   setState(() {
-  //     _orders = data['records'];
-  //   });
-  // }
-
   @override
   void initState() {
-    // readJson();
     super.initState();
   }
 
@@ -36,12 +24,22 @@ class _HomePagesState extends State<HomePages> {
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: ElevatedButton(
-              onPressed: () async {
-                final data = await service.generatePdf();
-                service.savePdfFile("PLN PDF", data);
-              },
-              child: Text('Get Pdf')),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () async {
+                    final data = await service.generatePdf();
+                    service.savePdfFile("PLN PDF", data);
+                  },
+                  child: Text('Get Pdf')),
+              ElevatedButton(
+                  onPressed: () async {
+                    await service.exportExcel();
+                  },
+                  child: Text('Get Excel')),
+            ],
+          ),
         ),
       ),
     );
--- lib/service/pdf_service.dart
+++ lib/service/pdf_service.dart
@@ -1,11 +1,14 @@
 import 'dart:io';
 
+import 'package:excel/excel.dart';
 import 'package:flutter/material.dart';
 import 'package:open_file/open_file.dart';
+import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'dart:typed_data';
 import 'package:pdf/widgets.dart' as pw;
+import 'package:permission_handler/permission_handler.dart';
 
 class Service {
   Future<Uint8List> generatePdf() {
@@ -25,5 +28,34 @@ class Service {
     final file = File(filePath);
     await file.writeAsBytes(byteList);
     await OpenFile.open(filePath);
+  }
+
+  Future<void> exportExcel() async {
+    // Minta izin untuk menulis ke penyimpanan eksternal
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      await Permission.storage.request();
+    }
+
+    Excel excel = Excel.createExcel();
+    Sheet sheet = excel['Test Sheet'];
+
+    var cell = sheet
+        .cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: 0)); //A1
+    var cell2 = sheet.cell(CellIndex.indexByString("A2")); //A2
+
+    cell.value = const TextCellValue("Ini index ke 1");
+    cell2.value = const TextCellValue("Ini index ke 2");
+
+    var fileBytes = excel.save();
+
+    // Dapatkan direktori penyimpanan eksternal
+    final directory = await getExternalStorageDirectory();
+    String path = directory!.path;
+
+    // Buat file Excel di direktori penyimpanan eksternal
+    File(join('$path/output_file_name.xlsx'))
+      ..createSync(recursive: true)
+      ..writeAsBytesSync(fileBytes!);
   }
 }
